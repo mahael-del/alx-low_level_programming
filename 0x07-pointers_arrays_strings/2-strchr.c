f63a783bed83f6f6b6206e49b6a6c5c4301df7c7
mahael-del <maha.elamraoui9@gmail.com>
2023-04-04 19:00:53 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
- * _strchr - Entry point
- * @s: input
- * @c: input
- * Return: Always NULL (Success)
+ * _strchr - find an occurance
+ * @s: str to be scanned
+ * @c: char to find
+ * Return: first occur or null
  */
 char *_strchr(char *s, char c)
 {
-        int i = 0;
+	int i;
 
-        for (; s[i] >= '\0'; i++)
-        {
-                if (s[i] == c)
-                        return (&s[i]);
-        }
-        return (NULL);
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (0);
 }
